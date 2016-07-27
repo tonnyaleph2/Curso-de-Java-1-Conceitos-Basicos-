1563b6c915b1028651f46c477ce36c16cfe66d1d
tonnyaleph2 <tonnyaleph@outlook.com>
2016-07-27 14:11:07 -0300

    Create Exemplo2Char.java

---
+++ Exemplo2Char.java
@@ -0,0 +1,9 @@
+public class Tipos {
+  public static void main(String[]args){
+    int numero_J=74;
+    char letra_J=(char)numero_J;
+    char letra_P=80;
+    char letra_i='i';
+    System.out.printf("%c%c%c%c %c%c%c%c%c%c%c%c%c%c%c \n",letra_J,97,118,97,letra_P,'r',111,103,114,101,'s','s',letra_i,118,111);
+  }
+}
