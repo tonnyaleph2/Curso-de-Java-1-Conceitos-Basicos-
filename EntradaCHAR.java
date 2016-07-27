1a2718eb35c1b840b468e3771fda18a1410bf4ed
tonnyaleph2 <tonnyaleph@outlook.com>
2016-07-27 14:22:51 -0300

    Create EntradaCHAR.java

---
+++ EntradaCHAR.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Tipos {
+  public static void main(String[]args){
+    Scanner entrada = new Scanner(System.in);
+    
+    char caractere;
+    caractere = entrada.nextLine().charAt(0); //coletar um caractere na entrada de dados
+  }
+}
+
+      
