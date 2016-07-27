e6fa3f1248a4a6f456ce7720496bb4946f0c5ebc
tonnyaleph2 <tonnyaleph@outlook.com>
2016-07-27 13:51:50 -0300

    Create TesteCharASCII.java

---
+++ TesteCharASCII.java
@@ -0,0 +1,9 @@
+public class caractere {
+  public static void main(String[]args){
+    char letra_J=74;
+    char letra_P=80;
+    
+    System.out.printf("%c %c \n",letra_J,letra_P);
+    
+  }
+}
