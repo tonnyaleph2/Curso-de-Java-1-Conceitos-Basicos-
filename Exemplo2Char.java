e5f249c8fe20b148eeade986eaf219b52cf69ad2
tonnyaleph2 <tonnyaleph@outlook.com>
2016-07-27 14:13:45 -0300

    Update Exemplo2Char.java

--- Exemplo2Char.java
+++ Exemplo2Char.java
@@ -1,9 +1,11 @@
 public class Tipos {
   public static void main(String[]args){
     int numero_J=74;
-    char letra_J=(char)numero_J;
+    char letra_J=(char)numero_J; /*Nao veja o numero_J como inteiro, veja como char. Apesar da declaracao estar como char.*/
     char letra_P=80;
     char letra_i='i';
     System.out.printf("%c%c%c%c %c%c%c%c%c%c%c%c%c%c%c \n",letra_J,97,118,97,letra_P,'r',111,103,114,101,'s','s',letra_i,118,111);
   }
 }
+
+      
